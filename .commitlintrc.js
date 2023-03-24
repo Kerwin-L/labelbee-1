8be80a0234e7b0e7c6c9c26ceaf590537dc8875c
laoluo <ron.f.luo@gmail.com>
2023-03-24 17:03:35 +0800

    chore(commitlint): Unify the commitlint of all packages

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,4 +1,27 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  rules: { 'subject-case': [2, 'always', ['sentence-case']] },
+  rules: {
+    'subject-case': [2, 'always', ['sentence-case']],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'update',
+        'fix',
+        'refactor',
+        'optimize',
+        'style',
+        'docs',
+        'chore',
+        'test',
+        'perf',
+        'revert',
+      ],
+    ],
+    "scope-empty": [
+      2,
+      "never"
+    ]
+  },
 };
